89b227fa30b17eb704e2d1e26eb39e7312d51533
lilalekat <lilalekat@yahoo.com>
2019-07-31 19:59:26 -0400

    updating readme., style.css, app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,6 +14,41 @@ var triviaQuestions = [
         answerChoices: ["Topanga", "Winny", "Blossom"],
         correctAnswer: "Topanga"
     },
+    {
+        question: "What was the quote that everyone seems to remember from Michelle Tanner in Full House?",
+        answerChoices: ["You got it, dude.", "Cut. It. Out", "How rude!"],
+        correctAnswer: "You got it, dude."
+    },
+    {
+        question: "What was the name of the name of the blonde love interest for Buffy in Buffy the Vampire Slayer?",
+        answerChoices: ["Angel", "Xander", "Spike"],
+        correctAnswer: "Spike"
+    },
+    {
+        question: "What was the candy that Kramer dropped over the side while watching a medical procedure in Seinfeld?",
+        answerChoices: ["Kit Kats", "Milk Duds", "Junior Mints"],
+        correctAnswer: "Junior Mints"
+    },
+    {
+        question: "What was the name of the school principle from Saved by the Bell?",
+        answerChoices: ["Mr. Belding", "Mr. Belvidere", "Mr. Burns"],
+        correctAnswer: "Mr. Belding"
+    },
+    {
+        question: "What was the name of Tim 'The Toolman' Taylor's handyman show from Home Improvement? ",
+        answerChoices: ["Tool Time", "Tool Rules", "Tool Tricks"],
+        correctAnswer: "Tool Time"
+    },
+    {
+        question: "What was the catchphrase that Steve Urkel was known for in the '90s sitcom, Family Matters?",
+        answerChoices: ["What does that mean?", "Did I do that?", "You know I can dig it!"],
+        correctAnswer: "Did I do that?"
+    },
+    {
+        question: "What is the name of the '90s sitcom that featured the 'Soup Nazi'?",
+        answerChoices: ["Full House", "Twin Peaks", "Seinfeld"],
+        correctAnswer: "Seinfeld"
+    },
 ];
 //Declaration of Variables for Timer, If/Else, ForLoop 
 var time = 60
@@ -31,7 +66,13 @@ for (var i = 0; i < triviaQuestions.length; i++) {
     questionP.text(triviaQuestions[i].question)
     $("#quizQuestions").append(questionP)
     for (var j = 0; j < triviaQuestions[i].answerChoices.length; j++) {
-     
-
-    }
+     $("#answerChoices").append(questionP)
 }
+$("#startQuizButton").on("click", function (){
+    $("#startQuizButton").replaceWith ();
+    console.log("quizStarted");
+}
+// $("#submitQuizButton").on("click", function (event) {
+    // event.preventDefault();
+// }
+)};
